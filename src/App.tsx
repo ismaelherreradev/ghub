367772a37e2ba63258436552c50b42e724f99155
ismaelherreradev <ismaelherrera.dr@outlook.com>
2024-09-01 12:34:59 -0400

    feat: dark-mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import "./App.css";
+import { ModeToggle } from "./components/mode-toggle";
 
 export default function App() {
   return (
     <>
+      <ModeToggle />
       <h1>HI</h1>
       <p>hello</p>
     </>
